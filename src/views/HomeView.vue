d8d22783863dfeecbc57a2f508d042dc41c469f1
Ava <D10816110@gs.takming.edu.tw>
2022-06-25 15:29:27 +0800

    fix: beautify portal card

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -8,15 +8,15 @@
 
     <v-row>
       <v-col md="4" cols="12">
-        <v-card height="150px">
-          <div>
+        <v-card class="d-flex flex-column px-5 py-3 rounded-lg" height="150px">
+          <div class="d-flex align-center flex-grow-0">
             <h3 class="primary--text"><strong>王小明</strong></h3>
-            <h4 class="gray--text">男</h4>
+            <h4 class="gray--text ml-auto">男</h4>
           </div>
-          <div>
+          <div class="flex-grow-0">
             <span class="gray--text">2021062011493428</span>
           </div>
-          <div>
+          <div class="flex-grow-1 d-flex align-end">
             <v-icon left color="seconsary">
               mdi-gmail
             </v-icon>
